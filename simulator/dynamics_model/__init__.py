a7f86fc139249ba7ba0e5a5bdb2d0ce48134e868
Yinda XU <ydxu_zju@hotmail.com>
2023-11-24 14:37:13 +0800

    [xyd/dynamics] add base class of dynamics model

---
+++ simulator/dynamics_model/__init__.py
@@ -0,0 +1,5 @@
+from .base_dynamics_model import BaseDynamicsModel
+
+__all__  = [
+    'BaseDynamicsModel',
+    ]
---
+++ simulator/dynamics_model/base_dynamics_model.py
@@ -0,0 +1,101 @@
+from typing import Union
+from abc import ABC, abstractmethod
+from copy import deepcopy
+
+import numpy as np
+from gym.spaces import Space
+
+
+DEFAULT_DTYPE = np.float32
+
+
+class BaseDynamicsModel(ABC):
+    """Base class for dynamics models
+
+    Serve as definitions of dynamics/transition functions.
+
+    Attributes:
+        state: vectorized state of the dynamics model
+        dtype: internal data type
+    """
+    state : np.ndarray
+    dtype : np.dtype
+
+    def __init__(
+        self,
+        init_state : np.ndarray=None,
+        dtype : np.dtype=DEFAULT_DTYPE,
+        ):
+        if init_state is not None:
+            self.set_state(deepcopy(init_state))
+        self.set_dtype(dtype)
+
+    def get_state(self) -> np.ndarray:
+        return deepcopy(self.state)
+
+    def set_state(self, new_state : np.ndarray):
+        self.state = deepcopy(new_state)
+
+    def get_dtype(self) -> np.dtype:
+        return self.dtype
+
+    def set_dtype(self, dtype : np.dtype):
+        self.dtype = dtype
+
+
+    @abstractmethod
+    def step(
+        self,
+        action : np.ndarray,
+        delta_t : float,
+        ):
+        """Step the model's state forward by a specified time interval
+
+        TODO: provide default implementation of `step` function (e.g. `step_without_update` + apply_delta_state)
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def compute_next_state(
+        self,
+        action : np.ndarray,
+        delta_t : float,
+        ) -> Any:
+        """
+        Proceed a step forward by a specified time interval
+            **without** update of internal state
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_dynamics_model_observation(self) -> np.ndarray:
+        """Make observation of the hyper-parameters of this dynamics model
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_state_observation(self) -> np.ndarray:
+        """Make observation of the state of this dynamics model
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_state_space(self,) -> Space:
+        """Get state space
+        """
+        raise NotImplementedError
+
+    @abstractmethod
+    def jacobian(self, state: np.ndarray, action: np.ndarray) -> np.ndarray:
+        """Compute jacobian with linearization
+
+        Perform the linearzation at a given state-action pair
+
+        Args:
+            state: the state at the linearization point, shape=(n_dims_state,)
+            action: the action at the linearization point, shape(n_dims_action,)
+
+        Returns:
+            jacobian matrix, shape=(n_dims_state, n_dims_state + n_dims_action)
+        """
+        raise NotImplementedError
