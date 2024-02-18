3360e5980da5e2555d4dd8d73a137b4002c8b821
Yinda Xu <ydxu_zju@hotmail.com>
2024-02-18 10:51:40 +0800

    [xyd/doc] add auto-generated Sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'DRL-based Trajectory Tracking (DRLTT)'
+copyright = '2024, Yinda Xu, Lidong Yu'
+author = 'Yinda Xu, Lidong Yu'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,20 @@
+.. DRL-based Trajectory Tracking (DRLTT) documentation master file, created by
+   sphinx-quickstart on Sun Feb 18 09:17:22 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to DRL-based Trajectory Tracking (DRLTT)'s documentation!
+=================================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
