7ab64229b1b4b7097b5a8d6beb7aff9d83e9a720
Yinda Xu <ydxu_zju@hotmail.com>
2024-02-22 15:04:38 +0800

    [xyd/script] fix bug in algorithm load

--- scripts/main.py
+++ scripts/main.py
@@ -13,6 +13,7 @@ from common import build_object_within_registry_from_config
 from common.io import load_config_from_yaml, convert_list_to_tuple_within_dict, override_config
 from simulator.rl_learning.sb3_learner import train_with_sb3, eval_with_sb3, build_sb3_algorithm_from_config
 from simulator.environments import ENVIRONMENTS
+from simulator.rl_learning.sb3_learner import SB3_MODULES
 
 
 def parse_args():
@@ -74,11 +75,7 @@ def main(args):
         eval_env_config = override_config(deepcopy(env_config), deepcopy(eval_config['overriden_environment']))
         eval_environment: Env = build_object_within_registry_from_config(ENVIRONMENTS, deepcopy(eval_env_config))
 
-        eval_algorithm: BaseAlgorithm = build_sb3_algorithm_from_config(
-            eval_environment,
-            config['algorithm'],
-        )
-        eval_algorithm.load(checkpoint_file_prefix)
+        eval_algorithm: BaseAlgorithm = SB3_MODULES[config['algorithm']['type']].load(checkpoint_file_prefix)
 
         eval_with_sb3(
             eval_environment,
