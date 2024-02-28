40830c5ba193a47cf49f72a64be00dcb19fc69d5
Yinda Xu <ydxu_zju@hotmail.com>
2024-02-28 21:16:32 +0800

    [xyd/sdk] common: add geometry utils

--- sdk/drltt-sdk/common/protobuf_operators.cpp
+++ sdk/drltt-sdk/common/protobuf_operators.cpp
@@ -1,12 +1,11 @@
-#pragma once
-
 #include "protobuf_operators.h"
+#include "geometry.h"
 
 drltt_proto::BodyState operator+(drltt_proto::BodyState lhs,
                                  const drltt_proto::BodyState& rhs) {
   lhs.set_x(lhs.x() + rhs.x());
   lhs.set_y(lhs.y() + rhs.y());
-  lhs.set_r(lhs.r() + rhs.r());
+  lhs.set_r(normalize_angle(lhs.r() + rhs.r()));
 
   return lhs;
 }
