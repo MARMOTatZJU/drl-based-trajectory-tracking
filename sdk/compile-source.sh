8da3ecf0b0c566549e8dc1ccb9283341fe6b86e1
Yinda Xu <ydxu_zju@hotmail.com>
2024-02-28 12:07:39 +0800

    [xyd/doc] add code snippets and fix typo
    - code snippets using `literalinclude` directive

--- sdk/compile-source.sh
+++ sdk/compile-source.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# shared libraries
 ld_lib_dir=/usr/local/lib
 export LD_LIBRARY_PATH=$ld_lib_dir:$LD_LIBRARY_PATH
 
@@ -7,11 +8,13 @@ export LD_LIBRARY_PATH=$ld_lib_dir:$LD_LIBRARY_PATH
 # https://stackoverflow.com/questions/19901934/libpthread-so-0-error-adding-symbols-dso-missing-from-command-line
 export LDFLAGS="-Wl,--copy-dt-needed-entries"
 
+# Check version and location of Protobuf compiler
 echo "Using protoc $(protoc --version) at $(which protoc)"
 
 rm -r ./build
 mkdir -p ./build
 pushd build
     cmake .. && make -j$(nproc --all)
-    cp -r $ld_lib_dir ./  # TODO: consider a more elegant way, like packaging
+    cp -r $ld_lib_dir ./    # export shared library.
+                            # TODO: consider a more elegant way, like packaging
 popd
