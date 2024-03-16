c006bae0abfc67081af7306a5286b1df35f23d0e
Yinda Xu <ydxu_zju@hotmail.com>
2024-03-16 14:32:40 +0800

    [xyd/test] fix test
    - sample config path
    - setting up code position (within subshell)

--- test-cpp.sh
+++ test-cpp.sh
@@ -3,9 +3,10 @@
 log_dir=./test-log
 mkdir -p $log_dir
 
-source setup.sh 2>&1 | tee ./${log_dir}/cpp-test-setup.log
 
 (
+    echo "SETTING UP CPP TESTING ENVIRONMENT"
+    source setup.sh
     echo "TEST CPP CODE"
     if [[ $1 == "fast"  ]]; then
         gen_data_script=./scripts/tests/train_eval_trace-track_test_sample_fast.sh
