a30b75f973dbf3847315cae8c9ee78db0e3c2f6f
Yinda Xu <ydxu_zju@hotmail.com>
2024-03-16 22:37:15 +0800

    [xyd/cfg] update var_reflen
    - update config of variable length of reference line

--- scripts/train_eval_trace-track_var_reflen.sh
+++ scripts/train_eval_trace-track_var_reflen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source setup.sh
-work_dir=work_dir/track-tiny-var-reflen
+work_dir=work_dir/track-var-reflen
 if [[ -d $work_dir ]];then
     bak_work_dir=${work_dir}-bak
     if [[ -d ${bak_work_dir} ]];then rm -rf ${bak_work_dir};fi
@@ -14,7 +14,7 @@ cp $script_path $work_dir/
 python tools/main.py \
     --config-files \
         configs/trajectory_tracking/config-track-base.yaml \
-        configs/trajectory_tracking/config-track-tiny.yaml \
+        configs/trajectory_tracking/config-track.yaml \
         configs/trajectory_tracking/config-track-var-reflen.yaml \
     --checkpoint-dir $work_dir/checkpoint \
     --num-test-cases 1024 \
